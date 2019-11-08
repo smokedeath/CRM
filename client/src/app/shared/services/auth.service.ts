2641c8f365270384610935d62a99f4d07ed574b8
k.zharenov <k.zharenov@kazdream.kz>
2019-11-08 19:09:25 +0600

    feat: front-end

---
+++ client/src/app/shared/services/auth.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from "@angular/core";
+import {User} from "../interfaces";
+import {HttpClient} from "@angular/common/http";
+import {tap} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private token = null;
+  constructor(private http: HttpClient) {}
+
+  login(user: User) {
+    return this.http.post('/api/auth/login', user)
+      .pipe(
+        tap(
+          (d) => {
+            const token = d.toString();
+            localStorage.setItem('auth-token', token);
+            this.setToken(token);
+          }
+        )
+      );
+  }
+
+  setToken(token: string) {
+    this.token = token;
+  }
+
+  getToken(): string {
+    return this.token;
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token;
+  }
+
+  logout() {
+    this.setToken(null);
+    localStorage.clear();
+  }
+
+  registration(){
+    //
+  }
+}
--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const controller = require('.././controllers/category');
 const passport = require('passport');
+const upload = require('./../middleware/upload');
 const router = express.Router();
 
 const jwt = passport.authenticate('jwt', {session: false});
@@ -8,7 +9,7 @@ const jwt = passport.authenticate('jwt', {session: false});
 router.get('/', jwt, controller.getAll);
 router.get('/:id', jwt, controller.getById);
 router.delete('/:id', jwt, controller.remove);
-router.post('/', jwt, controller.create);
-router.patch('/:id', jwt, controller.update);
+router.post('/', jwt, upload.single('image'), controller.create);
+router.patch('/:id', jwt, upload.single('image'), controller.update);
 
 module.exports = router;
